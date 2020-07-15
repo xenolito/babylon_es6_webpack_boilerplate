71f5f66d27b220d91c3413ebfc0d451f1050bae3
xenolito <oscar.rey.tajes@gmail.com>
2020-07-15 11:55:52 +0200

    initial setup after fork

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,16 @@
-console.log("Built!");
+import { Car } from "./js/modules/car.js";
+
+(function() {
+    console.log("hola");
+
+    var Micoche = new Car("Toyota", "RAV4");
+    Micoche.ponMarcha("D");
+    console.log(
+        "Marcha: " +
+            Micoche.userGear +
+            " \nMarca: " +
+            Micoche.marca +
+            " \nModelo: " +
+            Micoche.modelo
+    );
+})();
---
+++ app/src/js/modules/car.js
@@ -0,0 +1,22 @@
+class Car {
+    constructor(marca, modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this._userGears = ["P", "N", "R", "D"];
+        this._userGear = this._userGears[0];
+    }
+
+    get userGear() {
+        return this._userGear;
+    }
+    set userGear(gear) {
+        if (this._userGears.indexOf(gear) < 0) throw new Error(`Marcha no válida: ${gear}`);
+        this._userGear = gear;
+    }
+
+    ponMarcha(gear) {
+        this.userGear = gear;
+    }
+}
+
+export { Car };
